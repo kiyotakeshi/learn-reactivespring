950db43c4036aee34af38995003066e1fe5c968d
kiyotakeshi <kiyotatakeshi0609@gmail.com>
2020-02-18 00:43:31 +0900

    use lambda at end of Flux

--- src/main/java/com/learnreactivespring/fluxandmonoplayground/FlunAndmonoTest.java
+++ src/main/java/com/learnreactivespring/fluxandmonoplayground/FlunAndmonoTest.java
@@ -9,12 +9,14 @@ public class FlunAndmonoTest {
     public void fluxTest() {
 
         Flux<String> stringFlux = Flux.just("Spring", "Spring Boot", "Reactive Spring")
-                .concatWith(Flux.error(new RuntimeException("Exception Occurred")))
+//                .concatWith(Flux.error(new RuntimeException("Exception Occurred")))
                 .concatWith(Flux.just("After Error"))
                 .log();
 
-        stringFlux.subscribe(System.out::println,
-                (e) -> System.err.println("Exception is " + e));
+        stringFlux
+                .subscribe(System.out::println,
+                        (e) -> System.err.println("Exception is " + e)
+                        , () -> System.out.println("Completed"));
     }
 
 }
