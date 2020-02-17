e22ba1bd9559e8f19b111d2cabfce3943a7076a6
kiyotakeshi <kiyotatakeshi0609@gmail.com>
2020-02-18 00:29:11 +0900

    add Flux log and error check message detail

--- src/main/java/com/learnreactivespring/fluxandmonoplayground/FlunAndmonoTest.java
+++ src/main/java/com/learnreactivespring/fluxandmonoplayground/FlunAndmonoTest.java
@@ -9,10 +9,11 @@ public class FlunAndmonoTest {
     public void fluxTest() {
 
         Flux<String> stringFlux = Flux.just("Spring", "Spring Boot", "Reactive Spring")
-                .concatWith(Flux.error(new RuntimeException("Exception Occurred")));
+                .concatWith(Flux.error(new RuntimeException("Exception Occurred")))
+                .log();
 
         stringFlux.subscribe(System.out::println,
-                (e) -> System.err.println(e));
+                (e) -> System.err.println("Exception is " + e));
     }
 
 }
