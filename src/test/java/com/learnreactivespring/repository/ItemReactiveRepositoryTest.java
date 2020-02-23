ac0f6768438c7c620795b4b5264e6df6ec01cca3
kiyotakeshi <kiyotatakeshi0609@gmail.com>
2020-02-23 19:51:48 +0900

    executed test that add sample data in mongodb and find all

--- src/test/java/com/learnreactivespring/repository/ItemReactiveRepositoryTest.java
+++ src/test/java/com/learnreactivespring/repository/ItemReactiveRepositoryTest.java
@@ -1,11 +1,17 @@
 package com.learnreactivespring.repository;
 
+import com.learnreactivespring.document.Item;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
+
+import java.util.Arrays;
+import java.util.List;
 
 @DataMongoTest
 @RunWith(SpringRunner.class)
@@ -14,13 +20,38 @@ public class ItemReactiveRepositoryTest {
     @Autowired
     ItemReactiveRepository itemReactiveRepository;
 
+    List<Item> itemList = Arrays.asList(new Item(null, "Samsung TV", 400.0)
+            , new Item(null, "LG TV", 420.0)
+            , new Item(null, "Apple TV", 299.99)
+            , new Item(null, "Beats Headphones", 499.99));
+
+    @Before
+    public void setUp() {
+
+        itemReactiveRepository.deleteAll()
+                .thenMany(Flux.fromIterable(itemList))
+                .flatMap(itemReactiveRepository::save)
+                .doOnNext((item -> {
+                    System.out.println("Inserterd Item is :" + item);
+                }))
+                .blockLast();
+    }
+
     @Test
-    public void getAllItems(){
+    public void getAllItems() {
 
         StepVerifier.create(itemReactiveRepository.findAll()) // 0
                 .expectSubscription()
-                .expectNextCount(0)
+                .expectNextCount(4)
                 .verifyComplete();
     }
 
+    // Inserterd Item is :Item(id=5e52588a19e7203fc06d773f, description=LG TV, price=420.0)
+    // Inserterd Item is :Item(id=5e52588a19e7203fc06d7740, description=Apple TV, price=299.99)
+    // Inserterd Item is :Item(id=5e52588a19e7203fc06d7741, description=Beats Headphones, price=499.99)
+    // Inserterd Item is :Item(id=5e52588a19e7203fc06d773e, description=Samsung TV, price=400.0)
+    // 2020-02-23 19:48:42.756  INFO 17294 --- [extShutdownHook] org.mongodb.driver.connection            : Closed connection [connectionId{localValue:5, serverValue:5}] to localhost:27017 because the pool has been closed.
+    // 2020-02-23 19:48:42.756  INFO 17294 --- [extShutdownHook] org.mongodb.driver.connection            : Closed connection [connectionId{localValue:6, serverValue:6}] to localhost:27017 because the pool has been closed.
+    // 2020-02-23 19:48:42.756  INFO 17294 --- [extShutdownHook] org.mongodb.driver.connection            : Closed connection [connectionId{localValue:3, serverValue:3}] to localhost:27017 because the pool has been closed.
+    // 2020-02-23 19:48:42.757  INFO 17294 --- [extShutdownHook] org.mongodb.driver.connection            : Closed connection [connectionId{localValue:4, serverValue:4}] to localhost:27017 because the pool has been closed.
 }
