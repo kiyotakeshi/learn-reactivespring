ac5eb7d30a145ee003d3e7519965d1441fe23bc1
kiyotakeshi <kiyotatakeshi0609@gmail.com>
2020-02-24 15:33:37 +0900

    add other approach about get all items

--- src/test/java/com/learnreactivespring/controller/v1/ItemControllerTest.java
+++ src/test/java/com/learnreactivespring/controller/v1/ItemControllerTest.java
@@ -3,6 +3,7 @@ package com.learnreactivespring.controller.v1;
 import com.learnreactivespring.constants.ItemConstants;
 import com.learnreactivespring.document.Item;
 import com.learnreactivespring.repository.ItemReactiveRepository;
+import org.bson.codecs.IterableCodecProvider;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,9 +16,12 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
 
 import java.util.Arrays;
 import java.util.List;
+
+import static org.junit.Assert.assertTrue;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
@@ -52,7 +56,7 @@ public class ItemControllerTest {
     }
 
     @Test
-    public void getAllItems(){
+    public void getAllItems() {
         webTestClient.get().uri(ItemConstants.ITEM_END_POINT_V1)
                 .exchange()
                 .expectStatus().isOk()
@@ -61,4 +65,42 @@ public class ItemControllerTest {
                 .hasSize(4);
     }
 
+    @Test
+    public void getAllItemsApproach2() {
+        webTestClient.get().uri(ItemConstants.ITEM_END_POINT_V1)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBodyList(Item.class)
+                .hasSize(4)
+                .consumeWith((response) -> {
+                    List<Item> items = response.getResponseBody();
+                    items.forEach((item) -> {
+                        assertTrue(item.getId() != null);
+                    });
+                });
+    }
+
+    @Test
+    public void getAllItemsApproach3() {
+
+        Flux<Item> itemFlux = webTestClient.get().uri(ItemConstants.ITEM_END_POINT_V1)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .returnResult(Item.class)
+                .getResponseBody();
+
+        StepVerifier.create(itemFlux.log("value from network : "))
+                .expectNextCount(4)
+                .verifyComplete();
+
+        // 2020-02-24 15:20:03.423  INFO 2213 --- [           main] value from network :                     : | onSubscribe([Fuseable] FluxOnAssembly.OnAssemblySubscriber)
+        // 2020-02-24 15:20:03.432  INFO 2213 --- [           main] value from network :                     : | request(unbounded)
+        // 2020-02-24 15:20:03.447  INFO 2213 --- [           main] value from network :                     : | onNext(Item(id=5e536b12c96c4328d24e9773, description=Samsung TV, price=399.99))
+        // 2020-02-24 15:20:03.447  INFO 2213 --- [           main] value from network :                     : | onNext(Item(id=5e536b13c96c4328d24e9774, description=LG TV, price=329.99))
+        // 2020-02-24 15:20:03.447  INFO 2213 --- [           main] value from network :                     : | onNext(Item(id=5e536b13c96c4328d24e9775, description=Apple Watch TV, price=349.99))
+        // 2020-02-24 15:20:03.447  INFO 2213 --- [           main] value from network :                     : | onNext(Item(id=ABC, description=Beats HeadPhones , price=199.99))
+        // 2020-02-24 15:20:03.448  INFO 2213 --- [           main] value from network :                     : | onComplete()
+    }
 }
