51fdfc94f4a992d66d53fbf1b22fb45f85c64eaa
kiyotakeshi <kiyotatakeshi0609@gmail.com>
2020-02-24 17:05:10 +0900

    add create item test

--- src/test/java/com/learnreactivespring/controller/v1/ItemControllerTest.java
+++ src/test/java/com/learnreactivespring/controller/v1/ItemControllerTest.java
@@ -16,6 +16,7 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 import java.util.Arrays;
@@ -120,5 +121,21 @@ public class ItemControllerTest {
         webTestClient.get().uri(ItemConstants.ITEM_END_POINT_V1.concat("/{id}"), "DEF")
                 .exchange()
                 .expectStatus().isNotFound();
+    }
+
+    @Test
+    public void createItem(){
+
+        Item item = new Item(null, "iPhone X", 999.99);
+
+        webTestClient.post().uri(ItemConstants.ITEM_END_POINT_V1)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just(item), Item.class)
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody()
+                .jsonPath("$.id").isNotEmpty()
+                .jsonPath("$.description").isEqualTo("iPhone X")
+                .jsonPath("$.price").isEqualTo(999.99);
     }
 }
